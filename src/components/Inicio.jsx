cb78f05dd484c0428a0d17db0c1418fe7f93e11a
LegalBrian <legalbrian11@gmail.com>
2023-02-20 20:40:13 -0300

    card added and background in Inicio

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,6 +1,22 @@
+import foto from "../images/Foto.png"
+import inicioBg from '../images/Inicio-bg.jpg';
+
 const Inicio = () => {
+
+    const estilos = {
+        backgroundImage: `url(${inicioBg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    };
+    
     return (
-        <div>Inicio</div>
+        <div className="flex justify-center items-center brightness-75" style={estilos}>
+            <div className="bg-[rgba(37,51,66,0.7)] px-[20px] py-[50px] m-[20px] rounded-[40px] text-[#ffffff] brightness-200">
+                <img src={foto} className="h-[280px] w-[280px] rounded-full"/>
+                <p className="text-[40px] text-center">Brian Legal</p>
+                <p className="text-[25px] text-center">Full Stack Developer</p>
+            </div>
+        </div>
     )
 }
 
