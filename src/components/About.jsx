c77ca2be94c0b6a09ce6a70c6363b93f7f2892a5
LegalBrian <legalbrian11@gmail.com>
2023-02-22 21:32:52 -0300

    english corrected

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,11 +3,7 @@ const About = () => {
         <div id="about" className="bg-[rgb(0,0,25)] text-[#ffffff] text-center p-[40px]">
             <p className="text-[40px]">About me</p>
             <p className="text-[20px]">
-            Hi, my name is Brian Legal and I'm a Fullstack developer from Buenos Aires, Argentina. 
-            I consider myself a passionate developer and I am constantly learning. I love 
-            challenging projects and I'm always looking for opportunities to expand my skills and knowledge. 
-            my skills and knowledge. In my free time, I enjoy exploring new technologies. 
-            I am passionate about technology and am always ready to learn and grow as a developer.
+            Hi, my name is Brian Legal and I am a Full Stack Developer from Buenos Aires, Argentina. I think of myself as a passionate developer who is constantly learning, loves challenging projects, and is always looking for opportunities to expand their skills and knowledge. In my free time, I enjoy exploring new technologies, as I am truly fascinated by them. I can say for sure that I am ready to learn and grow as a developer, as well as to face any challenge that lies ahead.
             </p>
         </div>
     )
