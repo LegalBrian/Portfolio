c26d1167c25a606f6b790520ac2e5668d667dd96
Brian Legal <103913032+LegalBrian@users.noreply.github.com>
2023-04-25 16:52:01 -0300

    Update ExperienceList.js

--- src/components/lists/ExperienceList.js
+++ src/components/lists/ExperienceList.js
@@ -1,7 +1,7 @@
 export const education = [
     {
         company: "Henry", 
-        role: "Full Stack Developer", 
+        role: "Full Stack Web Developer", 
         date: "oct 2022 - feb 2023",
         description: "4 month live online bootcamp led by industry experts to develop programming skills in tools such as JavaScript, CSS, Node.js, React, and other technologies for professional development in front and back-end development.",
         certificate: "https://certificates.soyhenry.com/new-cert?id=9a77b7516b559a203972f141fe3e22bcb20bd4f4895e5a9a5b1ace0dde09255b",
