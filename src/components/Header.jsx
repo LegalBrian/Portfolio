2ffec314cece4f74b40db060889b5a9fadbd5253
LegalBrian <legalbriandev@gmail.com>
2023-06-25 23:24:28 -0300

    navbar responsive fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,75 +1,88 @@
 import logoB from "../images/logo-blanco.png"
-import { useState, useEffect, useContext } from "react";
-import { headerButtonMenu, headerButtonMenuSvg, headerContainer, headerLangs, headerNav, headerNavClosed, headerNavOpen, headerNavOption } from "./styleComponents";
+import { useState, useContext } from "react";
+import { headerLangs, headerNavOption } from "./styleComponents";
 import { FormattedMessage } from "react-intl";
 import { langContext } from "../context/langContext";
 import { ReactComponent as Spain } from '../images/Spain.svg';
 import { ReactComponent as USA } from '../images/USA.svg';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
-const Header = () => {
+const Navbar = () => {
 
     const lang = useContext(langContext);
+    const [nav, setNav] = useState(false);
 
-    const [menuOpen, setMenuOpen] = useState(false);
-    
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
+    const handleNav = () => {
+        setNav(!nav);
     };
 
-    useEffect(() => {
-        if (menuOpen) {
-            document.body.style.overflow = "hidden";
-        } else {
-            document.body.style.overflow = "";
-        }
-    }, [menuOpen]);
-
-    return(
-        <header className={headerContainer}>
-            <a href="/">
-                <img src={logoB} className="w-16 h-16" alt="Logo" />
+return (
+    <div className='flex justify-between items-center h-[80px] px-4 text-white bg-[rgba(0,0,0,1)]'>
+        <a href="/">
+            <img src={logoB} className="w-16 h-16" alt="Logo" />
+        </a>
+        <nav className='hidden desktop:flex laptop:flex'>
+            <a href={"/" + "#home"} className={headerNavOption}>
+                <FormattedMessage id="header.home" defaultMessage="INICIO" />
             </a>
-            <button className={`${headerButtonMenu} ${menuOpen ? '' : ''}`} onClick={toggleMenu}>
-                <span className="sr-only">Menú</span>
-                <svg className={headerButtonMenuSvg} viewBox="0 0 24 24">
-                    <rect x="4" y="5" width="16" height="2" />
-                    <rect x="4" y="11" width="16" height="2" />
-                    <rect x="4" y="17" width="16" height="2" />
-                </svg>
-            </button>
-
-            <nav className={`${headerNav} ${menuOpen ? headerNavOpen : headerNavClosed}`}>
-                <a onClick={toggleMenu} className="border cursor-pointer absolute top-[20px] right-0 self-end mr-[40px] border-gray-600 px-4 py-2 rounded text-white focus:outline-none desktop:hidden laptop:hidden">x</a>
-                <a href={"/" + "#home"} onClick={() => setMenuOpen(false)} className={headerNavOption}>
-                    <FormattedMessage id="header.home" defaultMessage="INICIO" />
-                </a>
-                <a href={"/" + "#about"} onClick={() => setMenuOpen(false)} className={headerNavOption}>
-                    <FormattedMessage id="header.about" defaultMessage="SOBRE MÍ" />
-                </a>
-                <a href={"/" + "#skills"} onClick={() => setMenuOpen(false)} className={headerNavOption}>                    
-                    <FormattedMessage id="header.skills" defaultMessage="HABILIDADES" />
-                </a>
-                <a href={"/" + "#resume"} onClick={() => setMenuOpen(false)} className={headerNavOption}>
-                    <FormattedMessage id="header.resume" defaultMessage="CURRICULUM" />
-                </a>
-                <a href={"/" + "#projects"} onClick={() => setMenuOpen(false)} className={headerNavOption}>
-                    <FormattedMessage id="header.projects" defaultMessage="PROYECTOS" />
-                </a>
-                <a href={"/" + "#contact"} onClick={() => setMenuOpen(false)} className={headerNavOption}>
-                    <FormattedMessage id="header.contact" defaultMessage="CONTACTO" />
-                </a>
-            </nav>
+            <a href={"/" + "#about"} className={headerNavOption}>
+                <FormattedMessage id="header.about" defaultMessage="SOBRE MÍ" />
+            </a>
+            <a href={"/" + "#skills"} className={headerNavOption}>                    
+                <FormattedMessage id="header.skills" defaultMessage="HABILIDADES" />
+            </a>
+            <a href={"/" + "#resume"} className={headerNavOption}>
+                <FormattedMessage id="header.resume" defaultMessage="CURRICULUM" />
+            </a>
+            <a href={"/" + "#projects"} className={headerNavOption}>
+                <FormattedMessage id="header.projects" defaultMessage="PROYECTOS" />
+            </a>
+            <a href={"/" + "#contact"} className={headerNavOption}>
+                <FormattedMessage id="header.contact" defaultMessage="CONTACTO" />
+            </a>
             <div className={headerLangs}>
-                    <a onClick={() => lang.setLang("es-AR")} className={`${lang.currentLang === "es-AR" ? "opacity-100 rounded-full border-[rgba(0,0,100,1)] border-[3px]" : "opacity-25"}`}>
-                        <Spain width="27" height="27"/>
-                    </a>
+                <a onClick={() => lang.setLang("es-AR")} className={`${lang.currentLang === "es-AR" ? "opacity-100 rounded-full border-[rgba(0,0,100,1)] border-[3px]" : "opacity-25"}`}>
+                    <Spain width="27" height="27"/>
+                </a>
+                <a onClick={() => lang.setLang("en-US")} className={`${lang.currentLang === "en-US" ? "opacity-100 rounded-full border-[rgba(0,0,100,1)] border-[3px]" : "opacity-25"}`}>
+                    <USA width="27" height="27"/>
+                </a>
+            </div>
+        </nav>
+        <div onClick={handleNav} className='block desktop:hidden laptop:hidden'>
+            {nav ? <></> : <AiOutlineMenu size={30} className="p-[5px] border-[1px] border-white rounded-[6px]"/>}
+        </div>
+        <nav className={nav ? 'fixed right-0 top-0 w-[60%] h-full border-l border-l-gray-900 bg-[#000300] ease-in-out duration-1000 flex flex-col justify-center items-center' : 'ease-in-out h-full top-0 duration-1000 fixed right-[-100%] flex flex-col justify-center items-center'}>
+            <AiOutlineClose size={30} onClick={handleNav} className="absolute top-[25px] right-[15px] self-end p-[5px] border-[1px] border-white rounded-[6px]"/>
+            <a href={"/" + "#home"} className={headerNavOption} onClick={handleNav}>
+                <FormattedMessage id="header.home" defaultMessage="INICIO" />
+            </a>
+            <a href={"/" + "#about"} className={headerNavOption} onClick={handleNav}>
+                <FormattedMessage id="header.about" defaultMessage="SOBRE MÍ" />
+            </a>
+            <a href={"/" + "#skills"} className={headerNavOption} onClick={handleNav}>                    
+                <FormattedMessage id="header.skills" defaultMessage="HABILIDADES" />
+            </a>
+            <a href={"/" + "#resume"} className={headerNavOption} onClick={handleNav}>
+                <FormattedMessage id="header.resume" defaultMessage="CURRICULUM" />
+            </a>
+            <a href={"/" + "#projects"} className={headerNavOption} onClick={handleNav}>
+                <FormattedMessage id="header.projects" defaultMessage="PROYECTOS" />
+            </a>
+            <a href={"/" + "#contact"} className={headerNavOption} onClick={handleNav}>
+                <FormattedMessage id="header.contact" defaultMessage="CONTACTO" />
+            </a>
+            <div className={headerLangs}>
+                <a onClick={() => lang.setLang("es-AR")} className={`${lang.currentLang === "es-AR" ? "opacity-100 rounded-full border-[rgba(0,0,100,1)] border-[3px]" : "opacity-25"}`}>
+                    <Spain width="27" height="27"/>
+                </a>
+                <a onClick={() => lang.setLang("en-US")} className={`${lang.currentLang === "en-US" ? "opacity-100 rounded-full border-[rgba(0,0,100,1)] border-[3px]" : "opacity-25"}`}>
+                    <USA width="27" height="27"/>
+                </a>
+            </div>
+        </nav>
+    </div>
+  );
+};
 
-                    <a onClick={() => lang.setLang("en-US")} className={`${lang.currentLang === "en-US" ? "opacity-100 rounded-full border-[rgba(0,0,100,1)] border-[3px]" : "opacity-25"}`}>
-                        <USA width="27" height="27"/>
-                    </a>
-                </div>
-        </header>
-    )
-}
-
-export default Header;
+export default Navbar;
