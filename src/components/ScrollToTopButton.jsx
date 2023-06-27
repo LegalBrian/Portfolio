e7c3f28febe6f29dcd8f6ef7e8e0c8bb89933fc8
LegalBrian <legalbriandev@gmail.com>
2023-06-27 16:47:30 -0300

    warnings fixed

--- src/components/ScrollToTopButton.jsx
+++ src/components/ScrollToTopButton.jsx
@@ -28,7 +28,7 @@ const ScrollToTopButton = () => {
   }, []);
 
   return (
-    <a
+    <button
       id="btnScrollToTop"
       onClick={scrollToTop}
       className={`fixed bottom-4 right-4 p-2 rounded-full flex items-center justify-center bg-[rgb(100,100,250)] w-[50px] h-[50px] transition-opacity duration-1000 ${
@@ -36,7 +36,7 @@ const ScrollToTopButton = () => {
       }`}
     >
       <Arrow className='self-center w-[40px] h-[40px]' />
-    </a>
+    </button>
   );
 };
 
